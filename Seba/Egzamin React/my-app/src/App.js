43a91432151edd42e0287ef56ab7971f29c57919
SebaProgramista <mrprofessoracc@gmail.com>
2023-01-02 09:48:48 +0100

    Update app.js

--- Seba/Egzamin React/my-app/src/App.js
+++ Seba/Egzamin React/my-app/src/App.js
@@ -1,21 +1,47 @@
+import React, { useState } from 'react';
+
 function App() {
+
+  var kursy = ["Programowanie w C#", "Angular dla początkujących", "Kurs Django"]
+
+  const [nameAndUsername, setNameAndUsername] = useState('')
+  const [nrCourse, setNrCourse] = useState('')
+
+  const nameAndUsernameChange = event => {
+    setNameAndUsername(event.target.value)
+  }
+  const nrCourseChange = event => {
+    setNrCourse(event.target.value)
+  }
+
+  function consoleOutput() {
+    console.log(nameAndUsername)
+    if (nrCourse > 0 && nrCourse - 1 < kursy.length && nrCourse != '') {
+      console.log(kursy[nrCourse - 1])
+    }
+    else
+    {
+      console.log("Nieprawidłowy numer kursu")
+    }
+  }
+
   return (
-    <div class="container">
+    <div className="container">
       <h2>Liczba kursów: 3</h2>
       <ol>
-        <li>Programowanie w C#</li>
-        <li>Angular dla początkujacych</li>
-        <li>Kurs Django</li>
+        {kursy.map(function (value, key){
+          return <li key={key}>{value}</li>
+        })}
       </ol>
-      <div class="form-group">
-        <label for="name-and-username">Imię i nazwisko</label>
-        <input type="text" className="form-control" id="name-and-username"/>
+      <div className="form-group">
+        <label htmlFor="name-and-username">Imię i nazwisko</label>
+        <input type="text" className="form-control" id="name-and-username" value={nameAndUsername} onChange={nameAndUsernameChange}/>
       </div>
-      <div class="form-group">
-        <label for="nr-course">Numer kursu</label>
-        <input type="text" className="form-control" id="nr-course"/>
+      <div className="form-group">
+        <label htmlFor="nr-course">Numer kursu</label>
+        <input type="text" className="form-control" id="nr-course" value={nrCourse} onChange={nrCourseChange}/>
       </div>
-      <button type="submit" className="btn btn-primary">Zapisz do kursu</button>
+      <button type="submit" className="btn btn-primary" onClick={consoleOutput}>Zapisz do kursu</button>
     </div>
   );
 }
